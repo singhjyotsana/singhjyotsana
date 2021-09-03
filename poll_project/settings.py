850be799ee5c64599f34a0d49a2ccf866ee94e48
Jyotsanasingh <singhjyotsana19@gmail.com>
2021-09-03 13:09:34 +0530

    Remove sec key

--- poll_project/settings.py
+++ poll_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0%hmgb&ts$w#(_#v8=1q!!w+9$mn&_@_@f_+rb76wb0zm_&2d+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
